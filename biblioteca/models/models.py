10723d7765b577cf81a6a10c62f579e818d4e3e6
Anahi-droid <lozaanahi02@gmail.com>
2025-10-08 17:17:08 -0500

    Actualización
    Commit Proyecto Odoo

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -4,16 +4,28 @@ from odoo import models, fields, api
 
 
 class biblioteca(models.Model):
-    _name = 'biblioteca.biblioteca'
+    _name = 'biblioteca.libro'
     _description = 'biblioteca.biblioteca'
-
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    _rec_name = 'lastname'
+    
+    firstname = fields.Char(sritng='Nombre Libro')
+    author = fields.Many2one('biblioteca.autor', string='Autor Libro') 
+    value = fields.Integer(string='Numero ejemplares')
+    value2 = fields.Float(compute="_value_pc", store=True, string="Costo")
+    description = fields.Text(string='Descripcion libro')
 
     @api.depends('value')
     def _value_pc(self):
         for record in self:
             record.value2 = float(record.value) / 100
 
+class BibliotecaAutor(models.Model):
+    _name= 'biblioteca.autor'
+    _description = 'biblioteca.autor'
+    _rec_name= 'firstname'
+    firstname = fields.Char()
+    lastname = fields.Char()
+    @api.depends('firstname', 'lastname')
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name= f"{record.firstname}{record.lastname}"
